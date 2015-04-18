03e5db09176321ae459e2edf6d000a4f86a03e1d
Michał Kowalski <michal.kowalski1992@gmail.com>
2015-04-18 14:41:18 +0200

    LeftToRight algorithm is now indepented from order in listener.getCopyOfLayerAsList()

--- src/production/algorithms/LeftToRight.java
+++ src/production/algorithms/LeftToRight.java
@@ -3,12 +3,23 @@ package production.algorithms;
 import production.PathPlanner;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LeftToRight extends PathPlanner {
     @Override
     protected List<Point> planPath() {
-        return listener.getCopyOfLayerAsList();
+        boolean[][] layer = listener.getCopyOfLayerAsSimpleTable();
+        List<Point> route = new ArrayList<>();
+
+        for (int i = 0; i < layer.length; i++) {
+            for (int j = 0; j < layer[i].length; j++) {
+                if (layer[i][j])
+                    route.add(new Point(i, j));
+            }
+        }
+
+        return route;
     }
 
     @Override
