ec1c584fc273ccdc2cecee29cba219ec532adf03
Michał Kowalski <michal.kowalski1992@gmail.com>
2015-05-03 16:49:05 +0200

    Reading multiple-param file

--- src/production/ParamReader.java
+++ src/production/ParamReader.java
@@ -1,7 +1,9 @@
 package production;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ParamReader {
@@ -43,5 +45,53 @@ public class ParamReader {
         }
 
         return params;
+    }
+
+    public static List<Map<String, String>> readTestFile(File file) {
+        List<Map<String, String>> paramList = new ArrayList<>();
+
+        FileInputStream stream = null;
+        try {
+            stream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assert stream != null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        String line;
+        Map<String, String> params = new HashMap<>();
+
+        try {
+            while ((line = br.readLine()) != null) {
+                if (line.length() == 0 || (line.charAt(0) == '#')) {
+                    continue;
+                }
+                line = line.split("#", 2)[0];
+
+                if (line.substring(0, 3).equals("***")) {
+                    paramList.add(params);
+                    params = new HashMap<>();
+                    continue;
+                }
+
+                String[] entry = line.split(": ");
+                if (entry.length != 2) {
+                    System.out.println("invalid line: " + line);
+                    continue;
+                }
+                params.put(entry[0], entry[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return paramList;
     }
 }
--- src/test/ParamReaderTest.java
+++ src/test/ParamReaderTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import production.ParamReader;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 
 public class ParamReaderTest {
@@ -12,9 +13,23 @@ public class ParamReaderTest {
     public void testReadAlgorithmFile() throws Exception {
         Map<String, String> params = ParamReader.readAlgorithmFile(new File("params/singleAlgorithmTest.txt"));
 
-        System.out.println("Start");
+        System.out.println("Single algorithm");
         for (Map.Entry<String, String> entry : params.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
+    @Test
+    public void testReadTestFile() throws Exception {
+        List<Map<String, String>> paramList = ParamReader.readTestFile(new File("params/test1.txt"));
+        System.out.println();
+        System.out.println("Test file");
+        System.out.println(paramList.size());
+        for (Map<String, String> params : paramList) {
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+            System.out.println();
         }
     }
 }
