1c9d71270399c4166090fd144e75789a4ab3fd49
Piotr Lechowicz <piotr.tobiasz.lechowicz@gmail.com>
2015-05-08 22:04:04 +0200

    Adding  thread in greedy algorithm

--- src/production/algorithms/Greedy.java
+++ src/production/algorithms/Greedy.java
@@ -3,31 +3,97 @@ package production.algorithms;
 import production.CostFunctionType;
 import production.MoveCostCalculator;
 import production.PathPlanner;
+import production.PathPlanningConnection;
 import production.algorithms.route.PrintingTool;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.List;
 
 public class Greedy extends PathPlanner {
 
-    private PrintingTool printingTool;
+    PrintingTool printingTool;
+    // domyslna wartosc, ustawiana jako parametr
     private final int NR_OF_THREADS = 10;
+    private int nrOfPoints;
+    // Array of starting points
+    private List<Point> startingPoints;
+    // Array of threads
+    private List<Thread> threads;
+
+    private List<FindPathThread> findPathThreads;
+
+    private List<List<Point>> listOfPaths;
 
     @Override
     protected void setUp() {
         printingTool = new PrintingTool(connection);
+        nrOfPoints = printingTool.getNumberOfPoints();
+
+        startingPoints = new ArrayList<>();
+        threads = new ArrayList<>();
+        findPathThreads = new ArrayList<>();
+        listOfPaths = new ArrayList<>();
     }
 
     @Override
     protected java.util.List<Point> planPath() {
-        FindPathThread thread1 = new FindPathThread(printingTool);
-        thread1.thread.start();
-        try {
-            thread1.thread.join();
-        } catch(InterruptedException ex){
-            System.out.println("exception");
-        };
-        System.out.println(MoveCostCalculator.calculate(printingTool.getRoute(), CostFunctionType.DISTANCE));
-        return thread1.printingTool.getRoute();
+        for (int i = 0; i < NR_OF_THREADS; i++)
+            startingPoints.add(printingTool.getPointFromList(nrOfPoints / NR_OF_THREADS * i));
+        for (Point point : startingPoints)
+            findPathThreads.add(new FindPathThread(connection, point));
+        for(FindPathThread path : findPathThreads)
+            threads.add(new Thread(path));
+        for (Thread thread : threads)
+            thread.start();
+
+
+//        ExecutorService exec = Executors.newCachedThreadPool();
+//        for (FindPathThread thread : threads)
+//            exec.execute(thread);
+        for(Thread thread : threads)
+            while(thread.getState() != Thread.State.TERMINATED);
+
+        for (FindPathThread path : findPathThreads)
+            listOfPaths.add(path.getRoute());
+
+
+        //DODAJ WYBOR STARTING POINTS
+        // ARRAY OF THREADS
+        //FindPathThread f = new FindPathThread(connection);
+        //Thread t = new Thread(f);
+        //t.start();
+        //while(t.getState() != Thread.State.TERMINATED) {}
+
+
+
+        return getBestPath(listOfPaths);
+    }
+
+    private List<Point> getBestPath(List<List<Point>> listOfRoutes) {
+        Iterator<List<Point>> it = listOfRoutes.iterator();
+        List<Point> best = null;
+        List<Point> point;
+        double bestDistance;
+        double distance;
+        if (it.hasNext()) {
+            best = it.next();
+            bestDistance = MoveCostCalculator.calculate(best, CostFunctionType.DISTANCE);
+
+            while (it.hasNext()) {
+                point = it.next();
+                distance = MoveCostCalculator.calculate(point, CostFunctionType.DISTANCE);
+                if (distance < bestDistance) {
+                    best = point;
+                    bestDistance = distance;
+                }
+            }
+        }
+        return best;
     }
 
     @Override
@@ -36,33 +102,26 @@ public class Greedy extends PathPlanner {
     }
 }
 
+
 class FindPathThread implements Runnable {
 
     PrintingTool printingTool;
-    Thread thread;
 
-    //Point startingPoint;
-    public FindPathThread(PrintingTool printingTool) {
-        //add properties for current threat
-        //this.startingPoint = startingPoint;
-        this.printingTool = printingTool;
-        thread = new Thread();
-
+    public FindPathThread(PathPlanningConnection connection) {
+        this.printingTool = new PrintingTool(connection);
+        findStartingPoint();
     }
 
+    public FindPathThread(PathPlanningConnection connection, Point startingPoint) {
+        this.printingTool = new PrintingTool(connection);
+        printingTool.setCurrentPosition(startingPoint);
+        printingTool.print();
+    }
 
     @Override
     public void run() {
-        //Find Path
-        findStartingPoint();
         while (printingTool.getNumberOfPoints() > 0)
             findNextPoint();
-        System.out.println(MoveCostCalculator.calculate(printingTool.getRoute(), CostFunctionType.DISTANCE));
-    }
-
-    private void findStartingPoint() {
-        printingTool.getFirstPoint();
-        printingTool.print();
     }
 
     private void findNextPoint() {
@@ -72,7 +131,7 @@ class FindPathThread implements Runnable {
 
         Point bestPoint = printingTool.getPointFromList(0);
         double currentDistance = calcDistance(currentPoint, bestPoint);
-        if (currentDistance < 1.00001 ) {
+        if (currentDistance == 1) {
             printingTool.print(bestPoint);
             return;
         }
@@ -91,7 +150,16 @@ class FindPathThread implements Runnable {
         printingTool.print(bestPoint);
     }
 
+    private void findStartingPoint() {
+        printingTool.getFirstPoint();
+        printingTool.print();
+    }
+
     private double calcDistance(Point first, Point second) {
         return Math.sqrt(Math.pow(second.getX() - first.getX(), 2) + Math.pow(second.getY() - first.getY(), 2));
+    }
+
+    public List<Point> getRoute() {
+        return printingTool.getRoute();
     }
 }
